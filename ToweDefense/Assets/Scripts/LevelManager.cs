eacee6ee34259fcf7a6f998171cb1f4ee8f8f6d4
Borodach-Ahmet <68484508+Borodach-Ahmet@users.noreply.github.com>
2021-03-23 01:15:57 +0300

    ADD main menu and menu for choice level

---
+++ ToweDefense/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    int levelUnLock;
+    public Button[] buttons;
+
+    public void loadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
