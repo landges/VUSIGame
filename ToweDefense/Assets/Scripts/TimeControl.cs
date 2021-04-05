5096a77578fc31ad5c297dc0a5674bccf062ee95
Borodach-Ahmet <68484508+Borodach-Ahmet@users.noreply.github.com>
2021-04-05 21:00:10 +0300

    Скорость уровня VUS-26

---
+++ ToweDefense/Assets/Scripts/TimeControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeControl : MonoBehaviour
+{
+    public Text speed;
+    public void Speed()
+    {
+        if (speed.text == "1x")
+        {
+            Time.timeScale = 1.5f;
+            speed.text = "1,5x";
+        }
+        else if (speed.text == "1,5x")
+        {
+            Time.timeScale = 2f;
+            speed.text = "2x";
+        }
+        else if (speed.text == "2x")
+        {
+            Time.timeScale = 1f;
+            speed.text = "1x";
+        }
+    }
+}
